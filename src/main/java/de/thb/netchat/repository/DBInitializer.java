a529b187c00366c2bc2967ad8e97516b313dc3aa
waldemarhermann <hermann.waldemar1995@gmail.com>
2025-11-09 21:48:49 +0100

    correct some typos

--- src/main/java/de/thb/netchat/repository/DBInitializer.java
+++ src/main/java/de/thb/netchat/repository/DBInitializer.java
@@ -4,12 +4,14 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/* Quelle: https://github.com/xerial/sqlite-jdbc */
+
 public class DBInitializer {
 
     public static void initialize() {
         String createUsers = """
                 create table if not exists users (
-                    id intger primary key autoincrement,
+                    id integer primary key autoincrement,
                     name text not null,
                     joined_at datetime default current_timestamp
                 );
@@ -17,10 +19,10 @@ public class DBInitializer {
 
         String createMessages = """
                 create table if not exists messages (
-                    id integer primary key autoincrement
+                    id integer primary key autoincrement,
                     sender_id integer not null,
                     text text not null,
-                    timestamp datetime default current_timestamp
+                    timestamp datetime default current_timestamp,
                     foreign key(sender_id) references users(id)
                 );
                 """;
